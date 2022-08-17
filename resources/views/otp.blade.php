78bc6b7a90aef51ca9ec0bdabbd98d119857b4db
Fnote Mehari <fnote.md@gmail.com>
2022-08-17 19:34:01 +0300

    otp test 4

--- resources/views/otp.blade.php
+++ resources/views/otp.blade.php
@@ -9,7 +9,7 @@
             <div class="col-md-8">
                 <div class="bg-white">
                     <h1>Hi {{$name}}!</h1>
-                    <h5>This is your OTP {{$name}}</h5>
+                    <h5>This is your OTP {{$otp}}</h5>
                 </div>
                 
             </div>
