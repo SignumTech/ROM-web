88796123415c2d64722d05aecfde3ff7c9c3e169
Fnote Mehari <fnote.md@gmail.com>
2022-08-17 11:31:43 +0300

    add navbar

---
+++ resources/js/components/app.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="row m-0">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white p-3">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><h2><strong>ROM</strong></h2></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item me-3">
+                        <a class="nav-link active" aria-current="page" href="#"><h5>WOMEN</h5></a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#"><h5>CURVE + PLUS</h5></a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#"><h5>KIDS</h5></a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#"><h5>MEN</h5></a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#"><h5>BEAUTY</h5></a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#"><h5>HOME</h5></a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" href="#"><h5>AFRICAN CLOTHING</h5></a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item me-4 dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"><h4><span class="fa fa-user"></span></h4></a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">SIGN IN / REGISTER</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">My orders</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item me-4">
+                        <a class="nav-link" href="#"><h4><span class="fa fa-shopping-bag"></span></h4></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><h4><span class="fa fa-heart"></span></h4></a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+<style>
+.dropdown-toggle::after {
+            content: none;
+        }
+</style>
