66b650d30da287163b92a44451e5b8f6a01344a3
Fnote Mehari <fnote.md@gmail.com>
2022-08-17 22:00:18 +0300

    otp test 8

--- resources/views/emails/otp/sent.blade.php
+++ resources/views/emails/otp/sent.blade.php
@@ -1,12 +1,9 @@
 @component('mail::message')
 <h1 style="font-size:28px; margin-top:10px; text-align:center">Welcome To ROM Fashion!! </h1> 
 
-<h4 style="text-align:center margin-top:50px;">Your OTP is</h4>
-<h1 style="font-size:38px; margin-top:20px; text-align:center"><strong>{{$otp}}</strong></h1>
+<h4 style="text-align:center; margin-top:50px;">Your OTP is</h4>
+<h1 style="font-size:38px; margin-top:10px; text-align:center"><strong>{{$otp}}</strong></h1>
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
 
 Thanks,<br>
 ROM Fashion
