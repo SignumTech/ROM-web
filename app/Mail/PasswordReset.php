4e6f5a828a760e0256472e7ff7e647c39a598118
Fnote Mehari <fnote.md@gmail.com>
2022-08-18 10:24:18 +0300

    add reset mailer and api

---
+++ app/Mail/PasswordReset.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class PasswordReset extends Mailable
+{
+    use Queueable, SerializesModels;
+    protected $user;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('no-reply@signumdev.com', 'ROM Fashion')
+                    ->markdown('emails.otp.reset')
+                    ->with([
+                        'name' => $this->user->name,
+                        'otp' => $this->user->otp
+                    ]);
+    }
+}
