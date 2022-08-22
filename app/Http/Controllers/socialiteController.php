6e1c0e4870120a3d4d31d1929d144ce13b3f8c9e
Fnote Mehari <fnote.md@gmail.com>
2022-08-22 09:29:48 +0300

    small fixes on socialite controller

--- app/Http/Controllers/socialiteController.php
+++ app/Http/Controllers/socialiteController.php
@@ -43,7 +43,7 @@ class socialiteController extends Controller
             Auth::login($user);
         }
         Auth::login($user);
-        return redirect('/sigin');
+        return redirect('/signin');
     }
 
     public function facebook_redirect(){
@@ -78,6 +78,6 @@ class socialiteController extends Controller
             Auth::login($user);
         }
         Auth::login($user);
-        return redirect('/sigin');
+        return redirect('/signin');
     }
 }
