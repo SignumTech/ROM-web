b524f283c4cbbeebc22219aa035e4d85f083043c
Fnote Mehari <fnote.md@gmail.com>
2022-09-23 22:41:11 +0300

    fix flash sale

--- resources/js/components/home/womenHome.vue
+++ resources/js/components/home/womenHome.vue
@@ -81,7 +81,7 @@ export default {
         PulseLoader,
         FlashCard,
         FlipCountdown,
-        flashProducts:{}
+        flashProducts:[]
     },
     mounted(){
         this.getCatByName()
